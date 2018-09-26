a82a4dd28999e3050c052dd0ab4114b87ac32bae
Andrew Coleman <penguincoder@gmail.com>
2018-09-26 18:09:10 -0400

    use downloadable one-time-use script

---
+++ bin/install-dotfiles.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#Initialization script sourced from Atlassian: https://developer.atlassian.com/blog/2016/02/best-way-to-store-dotfiles-git-bare-repo/
+git clone --bare git@github.com:penguincoder/dotfiles.git
+function config {
+  /usr/bin/git --git-dir=$HOME/dotfiles.git/ --work-tree=$HOME $@
+}
+mkdir -p .config-backup
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi;
+config checkout
+config config status.showUntrackedFiles no
+config push --set-upstream origin master
+config submodule init
+config submodule update
+fc-cache -f -v ~/.fonts
